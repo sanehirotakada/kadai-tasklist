0f419269a1f9e9bdfcecad450bbca4d1c6b22c79
sanehiro takada <sane.t@techacademy.jp>
2019-04-10 13:11:01 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   end
 
   def show
-    @tasks = Task.find(params[:id])  
+    @task = Task.find(params[:id])  
   end
 
   def new
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(message_params)
+    @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -20,6 +20,7 @@ class TasksController < ApplicationController
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new  
+    end
   end
 
   def edit
